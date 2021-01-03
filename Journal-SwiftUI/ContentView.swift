c9dc90793f028490f2d30867bded9b130ac49bb8
Bryan Workman <bryan@bryans-mbp.lan>
2021-01-02 17:53:34 -0800

    project start

---
+++ Journal-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Journal-SwiftUI
+//
+//  Created by Bryan Workman on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Journal-SwiftUI/Journal_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Journal_SwiftUIApp.swift
+//  Journal-SwiftUI
+//
+//  Created by Bryan Workman on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Journal_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
