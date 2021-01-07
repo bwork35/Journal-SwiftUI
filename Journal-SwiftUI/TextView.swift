260eb0bb14dad47d12fe1a6eb4174f28522c2bfd
Bryan Workman <bryan@bryans-mbp.lan>
2021-01-06 20:59:27 -0800

    start TextView setup

--- Journal-SwiftUI/TextView.swift
+++ Journal-SwiftUI/TextView.swift
@@ -5,11 +5,18 @@
 //  Created by Bryan Workman on 1/6/21.
 //
 
-//import SwiftUI
-//
-//struct TextView: UIViewRepresentable {
-//    typealias UIViewType = <#type#>
-//
-//
-//
-//}
+import SwiftUI
+
+struct TextView: UIViewRepresentable {
+    typealias UIViewType = UITextView
+
+    func makeUIView(context: Context) -> UITextView {
+        let textView = UITextView()
+        textView.textContainer.lineFragmentPadding = 0
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        
+    }
+}
